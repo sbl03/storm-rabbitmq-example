66cb4321e5ac77d4d90d867dded9039ad26346af
Liu, Shabai <SLiu@cvent.com>
2014-07-28 13:29:42 -0400

    Improve readability and set async config option

--- src/main/java/com/cvent/storm/rabbitmq/test/Main.java
+++ src/main/java/com/cvent/storm/rabbitmq/test/Main.java
@@ -27,6 +27,8 @@ import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CountDownLatch;
+import java.util.logging.Level;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -42,69 +44,90 @@ public class Main {
     private static final String RABBITMQ_PASS = "guest";
     private static final String EXCHANGE_NAME = "storm-test-exchange";
     private static final String QUEUE_NAME = "storm-test-queue";
-    private static final int NUM_MESSAGES = 10000;
+    private static final int NUM_MESSAGES = 100000;
     private static final String MESSAGE = "test message";
     private static final boolean PRODUCE = true;
-    
+
+    // Will publish and consume at the same time if true
+    private static final boolean ASYNC_PROCESSES = false;
+
     // Will publish a message everytime it consumes if true
     private static final boolean SHOW_DEBUG_TOPOLOGY_MESSAGES = false;
 
     public static void main(String[] args) {
         // Initialize everything
-        Message msg = new Message(MESSAGE.getBytes());
-        ConnectionConfig connConfig = new ConnectionConfig(RABBITMQ_HOST, RABBITMQ_USER, RABBITMQ_PASS);
-        Declarator decl = new CustomDeclarator(EXCHANGE_NAME, QUEUE_NAME);
-        ProducerConfig pConfig = new ProducerConfigBuilder().connection(connConfig)
+        final Message msg = new Message(MESSAGE.getBytes());
+        final ConnectionConfig connConfig = new ConnectionConfig(RABBITMQ_HOST, RABBITMQ_USER, RABBITMQ_PASS);
+        final Declarator decl = new CustomDeclarator(EXCHANGE_NAME, QUEUE_NAME);
+        final ProducerConfig pConfig = new ProducerConfigBuilder().connection(connConfig)
                 .exchange(EXCHANGE_NAME)
                 .contentEncoding("UTF-8")
                 .contentType("text/plain")
                 .build();
-        ConsumerConfig spoutConfig = new ConsumerConfigBuilder().connection(connConfig)
+        final ConsumerConfig spoutConfig = new ConsumerConfigBuilder().connection(connConfig)
                 .queue(QUEUE_NAME)
                 .prefetch(200)
                 .requeueOnFail()
                 .build();
+        
+        final CountDownLatch latch = new CountDownLatch(1);
 
         if (PRODUCE) {
-            // Start producers
-            RabbitMQProducer producer = new RabbitMQProducer(decl);
-            producer.open(pConfig.asMap());
+            Thread t = new Thread() {
+                @Override
+                public void run() {
+                    // Start producers
+                    RabbitMQProducer producer = new RabbitMQProducer(decl);
+                    producer.open(pConfig.asMap());
 
-            LOGGER.info("Starting producer for {} messages...", NUM_MESSAGES);
+                    LOGGER.info("Starting producer for {} messages...", NUM_MESSAGES);
 
-            long start = System.currentTimeMillis();
+                    long start = System.currentTimeMillis();
 
-            for (int i = 0; i < NUM_MESSAGES; i++) {
-                producer.send(msg);
+                    for (int i = 0; i < NUM_MESSAGES; i++) {
+                        producer.send(msg);
+                    }
+
+                    long delta = System.currentTimeMillis() - start;
+
+                    LOGGER.info("Took: {} ms @ {} msgs/s", delta, String.format("%.2f",
+                            NUM_MESSAGES / (delta / 1000.0)));
+                    
+                    latch.countDown();
+                }
+            };
+            t.start();
+
+            if (!ASYNC_PROCESSES) {
+                try {
+                    latch.await();
+                } catch (InterruptedException ex) {
+                    java.util.logging.Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+                }
             }
 
-            long delta = System.currentTimeMillis() - start;
+            LOGGER.info("Start up the topology...");
 
-            LOGGER.info("Took: {} ms @ {} msgs/s", delta, String.format("%.2f", NUM_MESSAGES / (delta / 1000.0)));
+            Scheme scheme = new Main.CustomScheme();
+            IRichSpout spout = new RabbitMQSpout(scheme, decl);
+
+            TopologyBuilder builder = new TopologyBuilder();
+            builder.setSpout("myspout", spout)
+                    .addConfigurations(spoutConfig.asMap())
+                    .setMaxSpoutPending(200);
+
+            Config conf = new Config();
+            conf.setDebug(SHOW_DEBUG_TOPOLOGY_MESSAGES);
+
+            // Start a local cluster
+            LocalCluster cluster = new LocalCluster();
+            cluster.submitTopology("test", conf, builder.createTopology());
+
+            // The topology will run indefinitely...
+            LOGGER.info("Done with everything.");
         }
-
-        LOGGER.info("Start up the topology...");
-        
-        Scheme scheme = new Main.CustomScheme();
-        IRichSpout spout = new RabbitMQSpout(scheme, decl);
-
-        TopologyBuilder builder = new TopologyBuilder();
-        builder.setSpout("myspout", spout)
-                .addConfigurations(spoutConfig.asMap())
-                .setMaxSpoutPending(200);
-
-        Config conf = new Config();
-        conf.setDebug(SHOW_DEBUG_TOPOLOGY_MESSAGES);
-
-        // Start a local cluster
-        LocalCluster cluster = new LocalCluster();
-        cluster.submitTopology("test", conf, builder.createTopology());
-        
-        // The topology will run indefinitely...
-
-        LOGGER.info("Done with everything.");
     }
-
+    
     private static class CustomScheme implements Scheme {
 
         @Override
